b36590ca689a5a0bc878a395fda897f130b9d6b2
shqawthmw060-sketch <shqawthmw060@gmail.com>
2026-08-25 18:14:22 +0000

    Add Android Capacitor build target

---
+++ android/app/src/main/java/com/monster/iptv/MainActivity.java
@@ -0,0 +1,5 @@
+package com.monster.iptv;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.monsteriptv.app',
+  appName: 'MONSTER IPTV',
+  webDir: 'dist/public',
+};
+
+export default config;
